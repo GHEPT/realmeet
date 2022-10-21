de522902f9d32c709da6079578ed42aa6749fb1d
Eduardo_SAP-Ecomm <eduardo.teodoro@cantustore.com.br>
2022-10-21 09:38:03 -0300

    > Outros cenários de testes de integração (sala inativa e sala que não existe) > Ajuste no RoomService para não trazer salas inativas

--- service/src/test/java/br/com/sw2you/realmeet/integration/RoomApiIntegrationTest.java
+++ service/src/test/java/br/com/sw2you/realmeet/integration/RoomApiIntegrationTest.java
@@ -1,8 +1,8 @@
 package br.com.sw2you.realmeet.integration;
 
+import static br.com.sw2you.realmeet.utils.TestConstants.DEFAULT_ROOM_ID;
 import static br.com.sw2you.realmeet.utils.TestDataCreator.newRoomBuilder;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import br.com.sw2you.realmeet.api.facade.RoomApi;
@@ -10,19 +10,21 @@ import br.com.sw2you.realmeet.api.model.RoomDTO;
 import br.com.sw2you.realmeet.core.BaseIntegrationTest;
 import br.com.sw2you.realmeet.domain.entity.Room;
 import br.com.sw2you.realmeet.domain.repository.RoomRepository;
+import br.com.sw2you.realmeet.utils.TestConstants;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.HttpClientErrorException;
 
 class RoomApiIntegrationTest extends BaseIntegrationTest {
     @Autowired
-    private RoomApi roomApi;
+    private RoomApi api;
 
     @Autowired
     private RoomRepository roomRepository;
 
     @Override
     protected void setupEach() throws Exception {
-        setLocalhostBasePath(roomApi.getApiClient(), "/v1");
+        setLocalhostBasePath(api.getApiClient(), "/v1");
     }
 
     @Test
@@ -33,9 +35,23 @@ class RoomApiIntegrationTest extends BaseIntegrationTest {
         assertNotNull(room.getId());
         assertTrue(room.getActive());
 
-        RoomDTO dto = roomApi.getRoom(room.getId());
+        RoomDTO dto = api.getRoom(room.getId());
         assertEquals(room.getId(), dto.getId());
         assertEquals(room.getName(), dto.getName());
         assertEquals(room.getSeats(), dto.getSeats());
+    }
+
+    @Test
+    void testGetRoomInactive() {
+        Room room = newRoomBuilder().active(false).build();
+        roomRepository.saveAndFlush(room);
+
+        assertFalse(room.getActive());
+        assertThrows(HttpClientErrorException.NotFound.class, () -> api.getRoom(room.getId()));
+    }
+
+    @Test
+    void testGetRoomDoesNotExist() {
+        assertThrows(HttpClientErrorException.NotFound.class, () -> api.getRoom(DEFAULT_ROOM_ID));
     }
 }
