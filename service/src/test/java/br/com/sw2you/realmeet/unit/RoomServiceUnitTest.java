2171997ade4c5f963f5a6d4475c155e3db181263
Eduardo_SAP-Ecomm <eduardo.teodoro@cantustore.com.br>
2022-10-21 09:48:16 -0300

    > Mais cenários de testes unitários

--- service/src/test/java/br/com/sw2you/realmeet/unit/RoomServiceUnitTest.java
+++ service/src/test/java/br/com/sw2you/realmeet/unit/RoomServiceUnitTest.java
@@ -3,15 +3,19 @@ package br.com.sw2you.realmeet.unit;
 import static br.com.sw2you.realmeet.utils.MapperUtils.roomMapper;
 import static br.com.sw2you.realmeet.utils.TestConstants.DEFAULT_ROOM_ID;
 import static java.util.Optional.of;
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
+import java.util.Optional;
 import br.com.sw2you.realmeet.api.model.RoomDTO;
 import br.com.sw2you.realmeet.core.BaseUnitTeste;
 import br.com.sw2you.realmeet.domain.entity.Room;
 import br.com.sw2you.realmeet.domain.repository.RoomRepository;
+import br.com.sw2you.realmeet.exception.RoomNotFoundException;
 import br.com.sw2you.realmeet.service.RoomService;
 import br.com.sw2you.realmeet.utils.TestDataCreator;
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -28,13 +32,19 @@ class RoomServiceUnitTest extends BaseUnitTeste {
     }
 
     @Test
-    void testGetRoom() {
+    void testGetRoomSuccess() {
         Room room = TestDataCreator.newRoomBuilder().id(DEFAULT_ROOM_ID).build();
-        when(roomRepository.findById(DEFAULT_ROOM_ID)).thenReturn(of(room));
+        when(roomRepository.findByIdAndActive(DEFAULT_ROOM_ID, true)).thenReturn(of(room));
 
         RoomDTO dto = victim.getRoom(DEFAULT_ROOM_ID);
         assertEquals(room.getId(), dto.getId());
         assertEquals(room.getName(), dto.getName());
         assertEquals(room.getSeats(), dto.getSeats());
+    }
+
+    @Test
+    void testGetRoomNotFound() {
+        when(roomRepository.findByIdAndActive(DEFAULT_ROOM_ID, true)).thenReturn(Optional.empty());
+        assertThrows(RoomNotFoundException.class, () -> victim.getRoom(DEFAULT_ROOM_ID));
     }
 }
