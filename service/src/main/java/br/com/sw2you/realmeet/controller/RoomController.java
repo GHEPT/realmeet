15b079aa8fd79f1d96be78438954707495ef4b17
Eduardo_SAP-Ecomm <eduardo.teodoro@cantustore.com.br>
2022-10-24 19:11:58 -0300

    Fluxo básico de criação de Room

--- service/src/main/java/br/com/sw2you/realmeet/controller/RoomController.java
+++ service/src/main/java/br/com/sw2you/realmeet/controller/RoomController.java
@@ -3,11 +3,13 @@ package br.com.sw2you.realmeet.controller;
 import static java.util.concurrent.CompletableFuture.supplyAsync;
 
 import br.com.sw2you.realmeet.api.facade.RoomsApi;
+import br.com.sw2you.realmeet.api.model.CreateRoomDTO;
 import br.com.sw2you.realmeet.api.model.RoomDTO;
 import br.com.sw2you.realmeet.service.RoomService;
 import br.com.sw2you.realmeet.util.ResponseEntityUtils;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
+import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +26,11 @@ public class RoomController implements RoomsApi {
     @Override
     public CompletableFuture<ResponseEntity<RoomDTO>> getRoom(Long id) {
         return supplyAsync(() -> roomService.getRoom(id), controllersExecutor).thenApply(ResponseEntityUtils::ok);
+    }
+
+    @Override
+    public CompletableFuture<ResponseEntity<RoomDTO>> createRoom(@Valid CreateRoomDTO createRoomDTO) {
+        return supplyAsync(() -> roomService.createRoom(createRoomDTO), controllersExecutor)
+            .thenApply(ResponseEntityUtils::created);
     }
 }
